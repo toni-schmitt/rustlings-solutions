8be94dbae49f51c4668527c48ac6d20b3761cdaa
toni-schmitt <tonischmitt@online.de>
2022-11-23 15:28:36 +0100

    ADD (variables): Working Exercises 1-6

---
+++ exercises/variables/variables3.rs
@@ -0,0 +1,9 @@
+// variables3.rs
+// Execute `rustlings 
+
+fn main() {
+    let x: i32 = 32;
+	let y: f32 = 32.3;
+	let s: &str = "Hello wrld";
+    println!("Number {} {} {}", x, y, s);
+}
---
+++ exercises/variables/variables6.rs
@@ -0,0 +1,10 @@
+// variables6.rs
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
+
+
+const NUMBER: i32 = 3;
+fn main() {
+	let THIS = 5;
+
+    println!("Number {} {}", NUMBER, THIS);
+}
